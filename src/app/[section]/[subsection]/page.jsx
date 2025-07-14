be75c90ecbfe67e66fe963af81739a522c4d2374
Rahaman <shaikjameelurrahaman@gmail.com>
2025-07-15 03:04:17 +0530

    Fixed error

--- src/app/[section]/[subsection]/page.jsx
+++ src/app/[section]/[subsection]/page.jsx
@@ -110,7 +110,7 @@ export default function SubsectionPage({ params }) {
             <div className="bg-muted rounded-lg w-full overflow-hidden">
               <div style={{ width: '100%', height: '800px', margin: '2rem 0' }}>
                 <iframe
-                  src="http://localhost:8052/"
+                  src="https://geopulse-dash-backend1.up.railway.app/"
                   title="Dash App"
                   width="100%"
                   height="100%"
